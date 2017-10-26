2f645d59532890072fae893c43c880411da09a72
Dil Sim <dilyasom@gmail.com>
2017-10-26 09:29:24 -0700

    Currently working on login

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,38 +1,70 @@
 import React, { Component } from "react";
+import openSocket from "socket.io-client";
 import { Col, Row, Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
 import StartBtn from "../../components/StartBtn";
 import { Link } from "react-router-dom";
 import { FormBtn } from "../../components/Form";
 import API from "../../utils/API";
+import './login.css';
 
 class Login extends Component {
     state = {
         username: "",
         password: "",
-        status: ""
+        status: "",
+        socket: "",
+        action: ""
     };
 
     componentDidMount = () => {
 
+        const socket = openSocket("http://localhost:3001");
+        const login = this;
+		
+		socket.on("connect", function(data) {
+			socket.emit("join", "Hello Server from client id #");
+		});
+
+		socket.on("id", function(data) {
+            login.setState({ socket: data });
+		});
     }
 
-    handleUserSubmit = event => {
+   
+       handleUserSubmit = event => {
 
         event.preventDefault();
-
-        API.getUser({username: this.state.username, password: this.state.password})
-        .then(res => this.setState({status: res.data}))
-        // .then(console.log(this.state.status))
+        const login = this;
+        API.getUser({
+            username: this.state.username,
+            password: this.state.password,
+            socket: this.state.socket
+        })
+        .then(function (res) {
+            login.setState({status: res.data});
+            if (login.state.status === "username not found"){
+                    login.setState({ action: "Username not found. Please, sign up."});
+                    
+            }
+            if (login.state.status === "authenticated"){
+                
+                                    login.setState({ action: "User found. Enjoy your game!"});
+                                    <Link to="/game">Play </Link>
+            }
+            if (login.state.status === "invalid password"){
+                
+                                    login.setState({ action: "Invalid password. Please, try again"})
+            }
+        })
         .catch(err => console.log(err));
-
-        // API.submitUserInfo(this.state.username, this.state.password)
-        // .then(res => this.setState({ status: res.data }))
-        // .catch(err => console.log(err));
 
         // clear out input forms on submit
         this.setState({ username: "", password: "" });
     };
+    
+
+  
 
     handleInputChange = event => {
         const { name, value } = event.target;
@@ -44,6 +76,7 @@ class Login extends Component {
     render = () => {
         return (
             <Container fluid>
+                
                 <Row>
                     <Col size="md-12">
                         <Jumbotron>
@@ -52,7 +85,7 @@ class Login extends Component {
                             </div>
                             <div id="login-container">
                                 <form id="login-form">
-                                    <br />
+                                    <h3>Socket ID: {this.state.socket}</h3>
                                     <label>Username {this.state.username}</label><br />
                                     <input
                                         name="username"
@@ -68,12 +101,40 @@ class Login extends Component {
                                         value={this.state.password}
                                         placeholder="Password" />
                                     <br /><br />
-                                    <FormBtn
+                                    <FormBtn  data-toggle="modal" data-target="#exampleModalLong"
                                         disabled={(!this.state.username || !this.state.password)}
                                         onClick={this.handleUserSubmit}>
                                         Login
 				    	            </FormBtn>
-                                    <h2>{this.state.status}</h2>
+
+        {/* modal --start */}
+
+        <div className="modal fade" id="exampleModalLong" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+            <div className="modal-content" id="modalContent">
+                <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLongTitle"> <h2>{this.state.action}</h2></h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div className="modal-body">
+                {this.state.status}
+                </div>
+                <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                {/* <button type="button" className="btn btn-primary" >Save changes</button> */}
+                </div>
+            </div>
+            </div>
+        </div>
+        {/* modal --end */}
+
+
+                                   <Link to="/game"> 
+                                        {/* <h2>{this.state.status}</h2> */}
+                                        {/* <h2>{this.state.action}</h2> */}
+                                   </Link>
                                 </form>
                             </div>
                             <div id="sign-up">
