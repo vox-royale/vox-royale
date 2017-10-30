8655bc883bbeb243194b8894ccaca567eb9dd402
Michael Stange <mstange22@yahoo.com>
2017-10-30 10:31:00 -0700

    toUpperCase()

--- controllers/compare.js
+++ controllers/compare.js
@@ -1,8 +1,8 @@
 // string comparison function
 function compare(targetPhrase, userPhrase) {
 
-    targetTokens = tokenize(targetPhrase);
-    userTokens = tokenize(userPhrase);
+    targetTokens = tokenize(targetPhrase.toUpperCase());
+    userTokens = tokenize(userPhrase.toUpperCase());
 
     // total number of non-white characters in target phrase 
     const numTargetCharacters = targetPhrase.length - targetTokens.length + 1;
@@ -17,16 +17,19 @@ function compare(targetPhrase, userPhrase) {
             tokensMatched++;
             charactersMatched += targetTokens[i].length;
         }
+        else {
+            console.log("Unmatched token: " + i + ". \"" + targetTokens[i] + "\" (user token: \"" + userTokens[i] + "\")");
+        }
     }
 
-    let match = {   numTargetTokens: targetTokens.length,
-                    numMatchedTokens: tokensMatched,
-                    numCharactersMatched: charactersMatched,
-                    numTotalCharacters: numTargetCharacters,
-                    percentage: charactersMatched / numTargetCharacters
+    let matchData = {   numTargetTokens: targetTokens.length,
+                        numMatchedTokens: tokensMatched,
+                        numCharactersMatched: charactersMatched,
+                        numTotalCharacters: numTargetCharacters,
+                        percentage: charactersMatched / numTargetCharacters
     }
     // return an object with match info
-    return (match);
+    return (matchData);
 }
 
 // helper function to break up phrase into tokens (words)
